e33cc677e3fd9054274c743b9817986e9f04f57e
CoffeeDev7 <coffeescripted@gmail.com>
2025-01-18 19:40:35 +0530

    major css changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-// App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import { ProductProvider } from './contexts/ProductContext';
+import ProductDetail from './components/ProductDetail'; // Import ProductDetail
 import './App.css';
 
 const App = () => {
     return (
         <ProductProvider>
-            <div className="app-container">
-                <Header />
-                <ProductList />
-            </div>
+            <Router>  {/* Wrap everything in Router */}
+                <div className="app-container">
+                    <Header />
+                    <Routes>  {/* Use Routes to handle routes */}
+                        <Route path="/" element={<ProductList />} />  {/* Use element prop */}
+                        <Route path="/product/:id" element={<ProductDetail />} />  {/* Use element prop */}
+                    </Routes>
+                </div>
+            </Router>
         </ProductProvider>
     );
 };
