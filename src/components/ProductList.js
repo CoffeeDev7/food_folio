f1d3f6e482715f5ad674d032580090a81b5b868c
CoffeeDev7 <coffeescripted@gmail.com>
2025-01-19 18:16:44 +0530

    cors error

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -53,7 +53,9 @@ const ProductList = () => {
                     url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchQuery}&json=true&page=${page}`;
                 }
 
-                const response = await fetch(url);
+                const response = await fetch(url,{
+                    mode: 'no-cors',
+                });
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
